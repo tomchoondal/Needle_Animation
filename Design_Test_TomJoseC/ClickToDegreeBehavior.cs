bb3c2284c63f67f212fcf599de08d335f7dae709
Tom Jose <tjose@valorem.com>
2018-04-09 11:08:57 +0530

    Angle moved to behavior

--- Design_Test_TomJoseC/ClickToDegreeBehavior.cs
+++ Design_Test_TomJoseC/ClickToDegreeBehavior.cs
@@ -13,6 +13,19 @@ namespace Design_Test_TomJoseC
         private FrameworkElement button;
         int clickCount = 0;
 
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(ClickToDegreeBehavior), new PropertyMetadata(string.Empty));
+
+
         public double DegreeToRotate
         {
             get { return (double)GetValue(DegreeToRotateProperty); }
@@ -22,6 +35,17 @@ namespace Design_Test_TomJoseC
         // Using a DependencyProperty as the backing store for DegreeToRotate.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty DegreeToRotateProperty =
             DependencyProperty.Register("DegreeToRotate", typeof(double), typeof(ClickToDegreeBehavior), new PropertyMetadata(0.0));
+
+        public double TargetAngle
+        {
+            get { return (double)GetValue(TargetAngleProperty); }
+            set { SetValue(TargetAngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TargetAngle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TargetAngleProperty =
+            DependencyProperty.Register("TargetAngle", typeof(double), typeof(ClickToDegreeBehavior), new PropertyMetadata(0.0));
+        
 
         protected override void OnAttached()
         {
@@ -34,6 +58,7 @@ namespace Design_Test_TomJoseC
         {
             clickCount++;
             DegreeToRotate = clickCount * 10;
+            TargetAngle = double.Parse(Text);
         }
         
         protected override void OnDetaching()
